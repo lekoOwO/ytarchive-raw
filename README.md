038919870c958c27551f90437bf7171a0c778017
Leko <rexx0520@gmail.com>
2021-03-17 13:39:56 +0800

    feat: auto output naming, metadata embed

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
  - ffmpeg
  
- - python3
+ - python3 > 3.4
 
 ## Usage
 
 Get freg json file using the [bookmark script](https://gist.github.com/lekoOwO/c90c09409446e6c7663c489bf06dc649).
 
-And do `python index.py -i FREG_JSON_FILE -o output.mkv`
+And do `python index.py -i FREG_JSON_FILE`
 
 TADA!
--- index.py
+++ index.py
@@ -8,10 +8,16 @@ import os
 import tempfile
 import subprocess
 import shlex
+from datetime import date
+import re
+import itertools
 
 FAIL_THRESHOLD = 5
 RETRY_THRESHOLD = 3
 DEBUG = True
+ACCENT_CHARS = dict(zip('ÂÃÄÀÁÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖŐØŒÙÚÛÜŰÝÞßàáâãäåæçèéêëìíîïðñòóôõöőøœùúûüűýþÿ',
+                        itertools.chain('AAAAAA', ['AE'], 'CEEEEIIIIDNOOOOOOO', ['OE'], 'UUUUUY', ['TH', 'ss'],
+                                        'aaaaaa', ['ae'], 'ceeeeiiiionooooooo', ['oe'], 'uuuuuy', ['th'], 'y')))
 
 class SegmentStatus:
     def __init__(self):
@@ -50,15 +56,14 @@ def download_segment(base_url, seg, seg_status):
             if response.getcode() >= 300 or response.getcode() < 200:
                 return False
             
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            with tempfile.NamedTemporaryFile(delete=False, prefix="ytarchive_raw_",  suffix=".seg") as tmp_file:
                 shutil.copyfileobj(response, tmp_file)
                 seg_status.segs[seg] = tmp_file.name
             return True
                
     except urllib.error.HTTPError as e:
         if DEBUG:
-            print(f"[DEBUG] Failed: {e.code}")
-            print(f"[DEBUG] url: {target_url}")
+            print(f"[DEBUG] Seg {seg} Failed with {e.code}")
         return False
 
 def merge_segs(target_file, seg_status):
@@ -111,8 +116,51 @@ def main(url, target_file):
     seg_status.end_seg = seg - 1 # Current seg is not available.
     merge_thread.join() # Wait for merge finished
 
+# ===== utils =====
+def sanitize_filename(s, restricted=False, is_id=False):
+    """Sanitizes a string so it could be used as part of a filename.
+    If restricted is set, use a stricter subset of allowed characters.
+    Set is_id if this is not an arbitrary string, but an ID that should be kept
+    if possible.
+    """
+    def replace_insane(char):
+        if restricted and char in ACCENT_CHARS:
+            return ACCENT_CHARS[char]
+        if char == '?' or ord(char) < 32 or ord(char) == 127:
+            return ''
+        elif char == '"':
+            return '' if restricted else '\''
+        elif char == ':':
+            return '_-' if restricted else ' -'
+        elif char in '\\/|*<>':
+            return '_'
+        if restricted and (char in '!&\'()[]{}$;`^,#' or char.isspace()):
+            return '_'
+        if restricted and ord(char) > 127:
+            return '_'
+        return char
+
+    # Handle timestamps
+    s = re.sub(r'[0-9]+(?::[0-9]+)+', lambda m: m.group(0).replace(':', '_'), s)
+    result = ''.join(map(replace_insane, s))
+    if not is_id:
+        while '__' in result:
+            result = result.replace('__', '_')
+        result = result.strip('_')
+        # Common case of "Foreign band name - English song title"
+        if restricted and result.startswith('-_'):
+            result = result[2:]
+        if result.startswith('-'):
+            result = '_' + result[len('-'):]
+        result = result.lstrip('.')
+        if not result:
+            result = '_'
+    return result
+# ===== utils end =====
+
 if __name__ == "__main__":
     import sys
+    import pathlib
 
     try:
         # Parse params
@@ -122,6 +170,8 @@ if __name__ == "__main__":
             "iv": None,
             "ia": None
         }
+
+        input_data = None
 
         args = sys.argv[1:]
         if len(args):
@@ -133,15 +183,15 @@ if __name__ == "__main__":
     -i, --input [JSON_FILE]     Input JSON file. Do not use with -iv or -ia.
     -iv, --input-video [URL]    Input video URL. Use with -ia.
     -ia, --input-audio [URL]    Input audio URL. Use with -iv.
-    -o, --output [OUTPUT_FILE]  Output file path.
+    -o, --output [OUTPUT_FILE]  Output file path. Uses `YYYYMMDD TITLE (VIDEO_ID).mkv` by default.
                     """)
                     sys.exit()
                 if args[i] == "-i" or args[i] == "--input":
                     import json
                     with open(args[i+1], encoding='UTF-8') as f:
-                        tmp = json.load(f)
-                        param["iv"] = [*tmp["video"].values()][0]
-                        param["ia"] = [*tmp["audio"].values()][0]
+                        input_data = json.load(f)
+                        param["iv"] = [*input_data["video"].values()][0]
+                        param["ia"] = [*input_data["audio"].values()][0]
                     i += 1
                 elif args[i] == "-iv" or args[i] == "--input-video":
                     param["iv"] = args[i+1]
@@ -158,15 +208,23 @@ if __name__ == "__main__":
                 i += 1
 
             if param["output"] is None:
-                raise RuntimeError("Output param not found.")
+                if input_data is not None:
+                    try:
+                        param["output"] = f"{date.today().strftime('%Y%m%d')} {sanitize_filename(input_data['metadata']['title'])} ({input_data['metadata']['id']}).mkv"
+                    except Exception as e:
+                        raise RuntimeError("JSON Version should be > 1.0, please update to the latest grabber.")
+                else:
+                    raise RuntimeError("Output param not found.")
+            if pathlib.Path(param["output"]).suffix.lower() != ".mkv":
+                raise RuntimeError("Output should be a mkv file.")
             if param["ia"] is None or param["iv"] is None:
                 raise RuntimeError("Input data not sufficient. Both video and audio has to be inputed.")
 
-        tmp_video_f = tempfile.NamedTemporaryFile(delete=False)
+        tmp_video_f = tempfile.NamedTemporaryFile(delete=False, prefix="ytarchive_raw_",  suffix="_video")
         tmp_video = tmp_video_f.name
         tmp_video_f.close()
 
-        tmp_audio_f = tempfile.NamedTemporaryFile(delete=False)
+        tmp_audio_f = tempfile.NamedTemporaryFile(delete=False, prefix="ytarchive_raw_",  suffix="_audio")
         tmp_audio = tmp_audio_f.name
         tmp_audio_f.close()
 
@@ -183,9 +241,31 @@ if __name__ == "__main__":
 
         if DEBUG:
             print("[DEBUG] Download finished. Merging...")
+
+        if input_data is not None:
+            tmp_thumbnail = None
+            with urllib.request.urlopen(input_data['metadata']["thumbnail"]) as response:
+                with tempfile.NamedTemporaryFile(delete=False, prefix="ytarchive_raw_", suffix=".jpg") as tmp_file:
+                    shutil.copyfileobj(response, tmp_file)
+                    tmp_thumbnail = tmp_file.name
+            
+            ffmpeg_params = [
+                "-metadata", f"title=\"{input_data['metadata']['title']}\"",
+                "-metadata", f"comment=\"{input_data['metadata']['description']}\"",
+                "-metadata", f"author=\"{input_data['metadata']['channelName']}\"",
+                "-metadata", f"episode_id=\"{input_data['metadata']['id']}\"",
+
+                "-attach", f"'{tmp_thumbnail}'", 
+                "-metadata:s:t", "mimetype=image/jpeg", 
+                "-metadata:s:t", "filename=\"thumbnail.jpg\""
+            ]
+            ffmpeg_params = " ".join(ffmpeg_params)
+        else:
+            ffmpeg_params = ""
                 
-        output_file = param["output"]
-        cmd = f"ffmpeg -i '{tmp_video}' -i '{tmp_audio}' -c copy '{output_file}'"
+        cmd = f"ffmpeg -i '{tmp_video}' -i '{tmp_audio}' -c copy {ffmpeg_params} '{param['output']}'"
+        if DEBUG:
+            print(f"[DEBUG] ffmpeg command: {cmd}")
         p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
                 
@@ -201,5 +281,12 @@ if __name__ == "__main__":
         print("Program stopped.")
 
     finally:
-        os.remove(tmp_video)
-        os.remove(tmp_audio)
+        try:
+            os.remove(tmp_video)
+        except:
+            pass
+
+        try:
+            os.remove(tmp_audio)
+        except:
+            pass
